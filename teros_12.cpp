8b3d99b99066b81bcca20671667824f807bfcf1c
Muhammad Faiz <faiz.shukri@protonmail.com>
2022-11-11 00:51:12 +0800

    wip

--- teros_12.cpp
+++ teros_12.cpp
@@ -1,12 +1,114 @@
 #include "teros_12.h"
 #include "elapsedMillis.h"
-Teros12::Teros12(SDI12 *sdi)
+
+#define TEROS12_MEASURE_DELAY 100
+Teros12::Teros12(SDI12 *sdi, uint8_t address)
 {
     this->sdi = sdi;
+    this->address = address;
 }
-
+void Teros12::clearBuffer()
+{
+    memset(rBuffer, 0, sizeof(rBuffer));
+    rBufferIndex = 0;
+}
+void Teros12::setup(uint32_t pollingRate = 1000)
+{
+    this->pollingRate = pollingRate;
+    clearBuffer();
+}
+void Teros12::sendMeasure()
+{
+    // aM0!
+    sdi->write(address);
+    sdi->print("M!");
+}
+uint8_t Teros12::measure()
+{
+}
+void Teros12::sendRead()
+{
+    // aD0!
+    sdi->write(address);
+    sdi->print("D0!");
+}
+uint8_t Teros12::read()
+{
+}
 uint8_t Teros12::tick()
 {
+    /*
+    switch (step)
+    {
+    case 0:
+    {
+        step++;
+        break;
+    }
+    case 1: // wait next sample cycle
+    {
+        if (timer >= pollingRate)
+        {
+            timer = 0;
+            sendMeasure();
+            e = 0;
+            step++;
+        }
+
+        break;
+    }
+    case 2: // wait measurement completed
+    {
+        if (e >= HTU21D_READ_DELAY)
+        {
+            uint8_t res = readTemperature();
+            temperatureError = res;
+            if (res)
+                step = 0;
+            else
+                step++;
+        }
+        break;
+    }
+    case 3: // wait measurement completed
+    {
+        uint8_t res = requestHumidity();
+        e = 0;
+        if (res)
+        {
+            humidityError = res;
+            step = 0;
+        }
+        else
+            step++;
+
+        break;
+    }
+    case 4: // wait measurement completed
+    {
+        if (e >= HTU21D_READ_DELAY)
+        {
+            uint8_t res = readHumidity();
+            humidityError = res;
+            step = 0; // step++;
+        }
+        break;
+    }
+    default:
+        step = 0;
+        break;
+    }
+
+    if (step == 0)
+    {
+        error = temperatureError | humidityError;
+        if (error && onReadError != nullptr)
+            onReadError(this);
+        if (!error && onDataUpdated != nullptr)
+            onDataUpdated(this);
+    }
+    return step;
+    */
 }
 
 void Teros12::changeAddress(uint8_t newAddress)
--- teros_12.h
+++ teros_12.h
@@ -4,15 +4,29 @@
 #include "SDI12.h"
 class Teros12
 {
+private:
+    uint8_t rBuffer[24];
+    uint8_t rBufferIndex = 0;
+    uint8_t step;
+    elapsedMillis e;
+    elapsedMillis timer;
+    void clearBuffer();
+    void sendMeasure();
+    uint8_t measure();
+    void sendRead();
+    uint8_t read();
 public:
     float calibratedCountsVWC;
     float temperature;
     float electricalConductivity;
 
-    uint8_t address = '0';
     SDI12 *sdi;
-    Teros12(SDI12 *sdi);
+    uint8_t address = '0';
+    uint32_t pollingRate;
+
+    Teros12(SDI12 *sdi,uint8_t address = '0');
     uint8_t tick();
+    void setup(uint32_t pollingRate = 1000);
     void changeAddress(uint8_t newAddress);
 };
 
