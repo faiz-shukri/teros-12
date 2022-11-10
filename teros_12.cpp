291aaf291570ffe80fac2d36eaa4edb86affe7e6
Muhammad Faiz <faiz.shukri@protonmail.com>
2022-11-11 03:17:47 +0800

    workable

--- teros_12.cpp
+++ teros_12.cpp
@@ -1,7 +1,70 @@
 #include "teros_12.h"
 #include "elapsedMillis.h"
 
-#define TEROS12_MEASURE_DELAY 100
+#define TEROS12_MEASURE_DELAY 500
+#define TEROS12_READ_DELAY 500
+
+float parseFloat(const char *data, size_t head, size_t tail, uint16_t &lastIndex)
+{
+    float v = 0;
+    bool neg = false;
+    float dec = 0;
+    uint16_t i;
+    for (i = head; i <= tail; i++)
+    {
+        if (i == head)
+        {
+            if (data[i] == '-')
+            {
+                neg = true;
+                continue;
+            }
+            else if (data[i] == '+')
+                continue;
+        }
+
+        if (data[i] == '.' && !dec)
+        {
+            dec = 10;
+            continue;
+        }
+
+        if (data[i] >= '0' && data[i] <= '9')
+        {
+            if (!dec)
+                v = v * 10 + data[i] - '0';
+            else
+            {
+                v = v + ((data[i] - '0') / dec);
+                dec *= 10;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    lastIndex = i;
+    return v * (neg ? -1 : 1);
+}
+
+bool parseData(const char *data, float *dst, int count)
+{
+    uint16_t head = 0;
+    uint16_t tail = strlen(data);
+
+    int idx = 0;
+    uint16_t lastIdx = 0;
+    for (int i = 0; i < count; i++)
+    {
+        dst[idx++] = parseFloat(data, head, tail, lastIdx);
+        head = lastIdx;
+        if (idx >= count)
+            break;
+    }
+
+    return idx == count;
+}
 Teros12::Teros12(SDI12 *sdi, uint8_t address)
 {
     this->sdi = sdi;
@@ -12,7 +75,7 @@ void Teros12::clearBuffer()
     memset(rBuffer, 0, sizeof(rBuffer));
     rBufferIndex = 0;
 }
-void Teros12::setup(uint32_t pollingRate = 1000)
+void Teros12::setup(uint32_t pollingRate)
 {
     this->pollingRate = pollingRate;
     clearBuffer();
@@ -20,28 +83,61 @@ void Teros12::setup(uint32_t pollingRate = 1000)
 void Teros12::sendMeasure()
 {
     // aM0!
-    sdi->write(address);
-    sdi->print("M!");
+    char cmd[] = "aM!";
+    cmd[0] = address;
+    sdi->sendCommand((const char *)cmd);
 }
-uint8_t Teros12::measure()
-{
-}
+
 void Teros12::sendRead()
 {
     // aD0!
-    sdi->write(address);
-    sdi->print("D0!");
+    char cmd[] = "aD0!";
+    cmd[0] = address;
+    sdi->sendCommand((const char *)cmd);
 }
-uint8_t Teros12::read()
+uint8_t Teros12::readData()
 {
+    // validate string
+    // 0+1829.42+26.2+0
+
+    // Serial.println((const char *)rBuffer);
+    float pdata[4];
+    if (parseData((const char *)rBuffer, pdata, 4))
+    {
+        this->calibratedCountsVWC = pdata[1];
+        this->temperature = pdata[2];
+        this->electricalConductivity = pdata[3];
+
+        // Serial.printf("[0]: %0.3f [1]: %0.3f [2]: %0.3f\r\n", this->calibratedCountsVWC, this->temperature, this->electricalConductivity);
+        return 1;
+    }
+
+    return 0;
+}
+
+int Teros12::readToBuffer()
+{
+    elapsedMillis eRead = 0;
+    int dataRead = 0;
+    while (sdi->available() && eRead < 200)
+    {
+        dataRead++;
+        uint8_t c = sdi->read();
+        if (rBufferIndex >= sizeof(rBuffer))
+            continue;
+
+        rBuffer[rBufferIndex++] = c;
+    }
+    return dataRead;
 }
 uint8_t Teros12::tick()
 {
-    /*
+
     switch (step)
     {
     case 0:
     {
+        // Serial.println(__LINE__);
         step++;
         break;
     }
@@ -50,48 +146,64 @@ uint8_t Teros12::tick()
         if (timer >= pollingRate)
         {
             timer = 0;
+            clearBuffer();
             sendMeasure();
             e = 0;
+            // Serial.println(__LINE__);
             step++;
         }
 
         break;
     }
-    case 2: // wait measurement completed
+    case 2: // wait reply
     {
-        if (e >= HTU21D_READ_DELAY)
+
+        readToBuffer();
+        if (e >= TEROS12_MEASURE_DELAY)
         {
-            uint8_t res = readTemperature();
-            temperatureError = res;
-            if (res)
-                step = 0;
-            else
-                step++;
+            // Serial.println(__LINE__);
+            step++;
         }
+
         break;
     }
-    case 3: // wait measurement completed
+
+    case 3: // send read
     {
-        uint8_t res = requestHumidity();
-        e = 0;
-        if (res)
+        if (rBufferIndex == 0) // no reply from prev, abort!
         {
-            humidityError = res;
+            error = 1;
             step = 0;
+            break;
         }
-        else
-            step++;
+        clearBuffer();
+        sendRead();
+        e = 0;
+        // Serial.println(__LINE__);
+        step++;
 
         break;
     }
-    case 4: // wait measurement completed
+    case 4: // wait reply
     {
-        if (e >= HTU21D_READ_DELAY)
+        readToBuffer();
+        if (e >= TEROS12_READ_DELAY)
         {
-            uint8_t res = readHumidity();
-            humidityError = res;
-            step = 0; // step++;
+            // Serial.println(__LINE__);
+            step++;
         }
+        break;
+    }
+    case 5: // process read
+    {
+        if (rBufferIndex == 0) // no reply from prev, abort!
+            error = 1;
+        else if (readData())
+            error = 0;
+        else
+            error = 1;
+
+        step = 0;
         break;
     }
     default:
@@ -101,27 +213,31 @@ uint8_t Teros12::tick()
 
     if (step == 0)
     {
-        error = temperatureError | humidityError;
-        if (error && onReadError != nullptr)
-            onReadError(this);
+
+        if (error && onError != nullptr)
+            onError(this);
         if (!error && onDataUpdated != nullptr)
             onDataUpdated(this);
     }
     return step;
-    */
+}
+void Teros12::setCallback(Teros12Callback onDataUpdatedCallback, Teros12Callback onErrorCallback)
+{
+    this->onDataUpdated = onDataUpdatedCallback;
+    this->onError = onErrorCallback;
 }
 
-void Teros12::changeAddress(uint8_t newAddress)
-{
-    uint8_t cmd[5] = {address, 'A', newAddress, '!', 0};
-    address = newAddress;
-    sdi->sendCommand((const char *)cmd);
-    elapsedMillis e = 0;
-    while (e < 1000)
-    {
-        while (sdi->available())
-        {
-            Serial.write(sdi->read());
-        }
-    }
-}
+// void Teros12::changeAddress(uint8_t newAddress)
+// {
+//     uint8_t cmd[5] = {address, 'A', newAddress, '!', 0};
+//     address = newAddress;
+//     sdi->sendCommand((const char *)cmd);
+//     elapsedMillis e = 0;
+//     while (e < 1000)
+//     {
+//         while (sdi->available())
+//         {
+//             Serial.write(sdi->read());
+//         }
+//     }
+// }
--- teros_12.h
+++ teros_12.h
@@ -2,6 +2,9 @@
 #define _teros_12_h
 #include "Arduino.h"
 #include "SDI12.h"
+#include "elapsedMillis.h"
+class Teros12;
+typedef void (*Teros12Callback)(Teros12 *);
 class Teros12
 {
 private:
@@ -11,23 +14,28 @@ private:
     elapsedMillis e;
     elapsedMillis timer;
     void clearBuffer();
+    int readToBuffer();
     void sendMeasure();
-    uint8_t measure();
     void sendRead();
-    uint8_t read();
+    uint8_t readData();
+
 public:
     float calibratedCountsVWC;
     float temperature;
     float electricalConductivity;
 
+    Teros12Callback onDataUpdated = nullptr;
+    Teros12Callback onError = nullptr;
+
     SDI12 *sdi;
     uint8_t address = '0';
     uint32_t pollingRate;
-
-    Teros12(SDI12 *sdi,uint8_t address = '0');
+    uint8_t error = 0;
+    Teros12(SDI12 *sdi, uint8_t address = '0');
     uint8_t tick();
     void setup(uint32_t pollingRate = 1000);
-    void changeAddress(uint8_t newAddress);
+    void setCallback(Teros12Callback onDataUpdatedCallback, Teros12Callback onErrorCallback);
+    // void changeAddress(uint8_t newAddress);
 };
 
 #endif
